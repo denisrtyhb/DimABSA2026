ffece0e280a1dca680725e1817c05fce053bd01a
Denis Rtyhb <denisrtyhb@gmail.com>
2026-04-26 21:42:24 +0300

    fixed exp2

--- run_exp2.sh
+++ run_exp2.sh
@@ -1,6 +1,6 @@
 python my_code/train.py --model_name bert-base-multilingual-cased \
     --batch_size 64 \
-    --epochs 4 \
+    --epochs 3 \
     --lr 2e-5 \
     --max_length 256 \
     --device cuda \
